757fe4befc7f085dc2988552b019ecc94cdbd7e4
Beqa Basiladze <64428073+BeqaBasiladze@users.noreply.github.com>
2022-01-04 20:50:39 +0400

    Add files via upload

--- js/responsiveMenu.js
+++ js/responsiveMenu.js
@@ -1,8 +1,25 @@
-function myFunction() {
-	var x = document.getElementById("myMenu");
-	if (x.className === "menu") {
-		x.className += " responsive";
-	} else {
-		x.className = "menu";
+function burgerMenu(selector) {
+	let menu = $(selector);
+	let button = menu.find('.burger-menu__button');
+	let links = menu.find('.burger-menu__link');
+	let overlay = menu.find('.burger-menu__overlay');
+
+	button.on('click', (e) => {
+		e.preventDefault();
+		toggleMenu();
+	});
+
+	links.on('click', () => toggleMenu());
+	overlay.on('click', () => toggleMenu());
+
+	function toggleMenu() {
+		menu.toggleClass('burger-menu_active');
+		if (menu.hasClass('burger-menu_active')) {
+			$('body').css('overflow', 'hidden');
+		} else {
+			$('body').css('overflow', 'visible');
+		}
 	}
 }
+
+burgerMenu('.burger-menu');
